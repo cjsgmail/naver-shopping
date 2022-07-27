e02dc47e06baa56e29a61212ffbdeea40d7a09a8
cjsgmail <cjs0097@gmail.com>
2022-07-28 01:42:42 +0900

    네이버 검색 쇼핑 api 불러오기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState([]);
+  const shoppingData = async () => {
+    const URL = "/v1/search/shop.json";
+    const ClientID = "I42Rx7d9Ib8CuJ95K2aP";
+    const ClientSecret = "PZ71hnBsJ7";
+
+    await axios
+      .get(URL, {
+        params: {
+          query: "모자",
+          display: 20,
+        },
+        headers: {
+          "X-Naver-Client-Id": ClientID,
+          "X-Naver-Client-Secret": ClientSecret,
+        },
+      })
+      .then((res) => setData(res.data.items))
+      .catch((e) => {});
+  };
+
+  useEffect(() => {
+    shoppingData();
+  }, []);
+
+  return <></>;
 }
 
 export default App;
