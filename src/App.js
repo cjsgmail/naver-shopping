9226ddb9c0e7bcaa080a4dbc916889e9e5575b82
cjsgmail <cjs0097@gmail.com>
2022-08-09 02:19:35 +0900

    add: 검색기능구현, css 카드나열

--- src/App.js
+++ src/App.js
@@ -5,20 +5,19 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [data, setData] = useState([]);
+  const [item, setItem] = useState("신발");
   const shoppingData = async () => {
     const URL = "/v1/search/shop.json";
-    const ClientID = "I42Rx7d9Ib8CuJ95K2aP";
-    const ClientSecret = "PZ71hnBsJ7";
 
     await axios
       .get(URL, {
         params: {
-          query: "모자",
-          display: 20,
+          query: item,
+          display: 100,
         },
         headers: {
-          "X-Naver-Client-Id": ClientID,
-          "X-Naver-Client-Secret": ClientSecret,
+          "X-Naver-Client-Id": process.env.REACT_APP_ClientId,
+          "X-Naver-Client-Secret": process.env.REACT_APP_ClientSecret,
         },
       })
       .then((res) => setData(res.data.items))
@@ -27,9 +26,48 @@ function App() {
 
   useEffect(() => {
     shoppingData();
-  }, []);
+  }, [item]);
 
-  return <></>;
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    setItem(e.target.title.value);
+  };
+
+  return (
+    <div className="main">
+      <header>
+        <h1>Naver Shop Search</h1>
+      </header>
+      <form onSubmit={handleOnSubmit}>
+        <input
+          type="text"
+          placeholder="제품명"
+          name="title"
+          className="search--product"
+        ></input>
+        <input type="submit" className="search--button" value="search"></input>
+      </form>
+      <section>
+        <div className="product--container">
+          <ul className="products">
+            {data.map((el) => {
+              const title = el.title.slice(0, el.title.indexOf("<"));
+              return (
+                <a href={el.link} key={el.productId}>
+                  <li className="product--list">
+                    <div className="product--img--container">
+                      <img src={el.image} className="product--img"></img>
+                    </div>
+                    <div className="product--name">{title}</div>
+                  </li>
+                </a>
+              );
+            })}
+          </ul>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default App;
